f47f1f9d3726e72ac04510971c7ef14316a7f383
Theo Coombes <theocoombes06@gmail.com>
2022-01-16 20:53:00 +0000

    fix

--- parse_webdatasets.py
+++ parse_webdatasets.py
@@ -40,9 +40,9 @@ def main(clip_model_type: str, device: str, webdataset_dir: str, output_filename
 
             print(sample.get("caption"), sample.get("cap"))
             
-            json = json.loads(sample["json"].decode())
+            jsn = json.loads(sample["json"].decode())
             
-            captions = [json["caption"]]
+            captions = [jsn["caption"]]
 
             d["clip_embedding"] = i
             d["captions"] = captions
