f68b8b6b91db3aebd6f261ff92416c9c2fdc0ae5
Theo Coombes <theocoombes06@gmail.com>
2022-01-16 20:42:05 +0000

    test

--- parse_webdatasets.py
+++ parse_webdatasets.py
@@ -29,12 +29,12 @@ def main(clip_model_type: str, device: str, webdataset_dir: str, output_filename
     all_captions = []
     for tar in tqdm(tars, desc="generating embeddings from tars"):
         dataset = wds.WebDataset(str(tar.resolve()))
-        print(dataset)
-        raise dataset
         
         for sample in dataset:
-        
             d = {}
+            
+            print(sample["img"], type(sample["img"]))
+            print(sample["cap"], type(sample["cap"]))
 
             image = imagetransform(sample["img"])
             image = preprocess(image).unsqueeze(0).to(device)
